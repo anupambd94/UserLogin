8d1b638de40fdcc134bba0f979f554fc65410eb4
Anupam Hayat <anupam.bd94@gmail.com>
2019-02-06 01:16:57 +0600

    sdfsef

---
+++ UserLogin/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserLogin.Models;
+
+namespace UserLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public JsonResult UserLogin(user u)
+        //{
+        //    using (Entities en = new Entities())
+        //    {
+        //        var user = en.users.Where(a => a.username.Equals(u.username) && a.password.Equals(u.password)).FirstOrDefault();
+        //        return new JsonResult { Data = user, JsonRequestBehavior = JsonRequestBehavior.AllowGet};
+        //    }
+        //}
+	}
+}
